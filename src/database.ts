3ddc8a2e132ec358dcfaa76ff20fc72796d185b9
alej1286 <alej1286@gmail.com>
2021-12-18 13:30:27 -0500

    databse.ts change to connectionstring

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,11 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'master32',
-    database: 'akenton',
-    port: 5432
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
+    //user: 'postgres',
+    //host: 'localhost',
+    //password: 'master32',
+    //database: 'akenton',
+    //port: 5432
 });
