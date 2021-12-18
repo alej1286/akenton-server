bc898ebc1dd564ff98c739aad226ed832c763871
alej1286 <alej1286@gmail.com>
2021-12-18 14:28:05 -0500

    Searching error DB

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,8 @@ const router = Router();
 import { getOrders, getOrderById, createOrder, updateOrder, deleteOrder } from '../controllers/index.controller';
 
 router.get('/', (req, res) => {
-    res.send('<h1>This is the main /</h1>')
+    res.send('<h1>This is the main /</h1>');
+    console.log("Database_URL", process.env.DATABASE_URL);
 });
 router.get('/orders', getOrders);
 router.get('/orders/:id', getOrderById);
