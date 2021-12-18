900c2209a67e8ed7c6bd471240048a73537af1d0
alej1286 <alej1286@gmail.com>
2021-12-18 14:38:16 -0500

    testing routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ const router = Router();
 
 import { getOrders, getOrderById, createOrder, updateOrder, deleteOrder } from '../controllers/index.controller';
 
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     res.send('<h1>This is the main /</h1>');
     console.log("Database_URL", process.env.DATABASE_URL);
-});
-router.get('/orders', getOrders);
+    getOrders
+});*/
+router.get('/', getOrders);
+//router.get('/orders', getOrders);
 router.get('/orders/:id', getOrderById);
 router.post('/orders', createOrder);
 router.put('/orders/:id', updateOrder)
