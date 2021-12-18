bc12880059187f8902967572181a39e5d6fa361a
alej1286 <alej1286@gmail.com>
2021-12-18 13:51:48 -0500

    Change DB

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,9 @@ import { Pool } from 'pg';
 
 export const pool = new Pool({
     connectionString : process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false
+      }
     //user: 'postgres',
     //host: 'localhost',
     //password: 'master32',
