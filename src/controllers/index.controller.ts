4227ed2c01b01df7964243bef22e10a9f993fe3f
alej1286 <alej1286@gmail.com>
2022-01-31 21:11:50 -0500

    2

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -854,8 +854,8 @@ export const getWeekProductionStat = async (
   }; */
 
   //var startOfWeek = moment().startOf('isoweek').toDate();
-  var startOfWeek = getMonday(new Date());
-  startOfWeek.setHours(0,0,0,0);
+  var startOfWeek = getMonday(new Date().setHours(0, 0, 0, 0));
+  
   //var startOfWeek = getMonday(new Date("2022-01-24"));
   //var endOfWeek   = moment().endOf('isoweek').toDate();
   var endOfWeek   = new Date();
